0620dddb11f6087aabd1ff5d4215fada6d7a1d66
peter-sq <awe.petersq@gmail.com>
2025-01-09 16:36:18 +0100

    dashboard

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,15 +1,71 @@
-import { Menu } from 'lucide-react';
+"use client";
+import { LampFloorIcon, Layout, LayoutDashboard, Link, LucideIcon, Menu } from 'lucide-react';
 import React from 'react';
+import { useAppDispatch, useAppSelector } from '../../redux';
+import { setIsSidebarCollapsed } from '../../state';
+import { usePathname } from 'next/navigation';
+
+interface SidebarLinkProps{
+  href: string,
+  icon: LucideIcon,
+  label: string,
+  isCollapsed: boolean
+}
+
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+
+  return (
+    <Link href = {href}>
+      <div 
+      className={`cursor-pointer flex items-center ${
+        isCollapsed ? "justify-content py-4" : "justify-start px-8 py-4"
+      }
+        hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+          isActive ? "bg-blue-200 text-white" : ""
+        }
+        
+      }`}>
+        <Icon className='w-6 h-6 text-gray-700'/>
+        <span className={`${isCollapsed ? "hidden" : "block"}
+        font-medium text-gray-700`}>
+
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 
 const Sidebar = () => {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))
+  };
+  const sidebarClassName = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
     return (
         <div>
+          <div className={sidebarClassName}>
             {/* TOP SIDEBAR */}
-      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8 '>
+      <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+        isSidebarCollapsed ? "px-5" : "px-8"}`}>
         <div>logo</div>
-        <h1 className='font-extrabold text-2xl'>PETVOT</h1>
-      <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' >
+        <h1 className={` font-extrabold text-2xl
+          ${isSidebarCollapsed ? "hidden" : "block"}`
+        }>PETVOT</h1>
+      <button className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100'
+      onClick={toggleSidebar} >
       <Menu className='w-4 h-4'/>
       </button>
       </div>
@@ -17,12 +73,18 @@ const Sidebar = () => {
              {/* Links */}
              <div className='flex-grow mt-8'>
                 {/* links here */}
+                <SidebarLink
+                 href='/dashboard' 
+                 icon={LampFloorIcon} 
+                 label='Dashboard' 
+                 isCollapsed={isSidebarCollapsed}/>
 
              </div>
              {/* FOOTER */}
              <div>
                 <p className='text-center text-xs text-gray-500'>&copy; 2024 PETVOT</p>
              </div>
+      </div>
       </div>
     )
 }
